6b70a392cd9c85cec5757ee8a60bd160c084da99
catherine <catherine5th.avenue@gmail.com>
2017-09-27 17:30:53 +0900

    プラグインのひな形を作成

---
+++ vk-call-to-action.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Vk Call To Action
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     vk-call-to-action
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Vk_Call_To_Action
+ */
